5b7586421b91a731a71417aa44cad9a208f19c03
PENGUINx15 <103443268+PENGUINx15@users.noreply.github.com>
2023-06-03 19:50:27 +0300

    Add files via upload

--- MobLoot/src/me/penguin/mobloot/mobloot.java
+++ MobLoot/src/me/penguin/mobloot/mobloot.java
@@ -15,6 +15,8 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Random;
 
@@ -28,14 +30,14 @@ public class mobloot extends JavaPlugin implements Listener {
     private String configReloadMessage;
     private String noPermissionMessage;
     private Map<Integer, EntityType> commandEntities;
-
+    private List<EntityType> blacklistedDropEgg;
+    private List<EntityType> blacklistedСurrencyDrop;
     private String getCurrencyMessage(EntityType entityType, int amount) {
         String message = getConfig().getString("CurrencyDrop.Msg");
         message = message.replace("{entity}", getFriendlyName(entityType));
         message = message.replace("{amount}", String.valueOf(amount));
         return message;
     }
-
     private String getFriendlyName(EntityType entityType) {
         return WordUtils.capitalizeFully(entityType.name().replace("_", " "));
     }
@@ -56,6 +58,28 @@ public class mobloot extends JavaPlugin implements Listener {
         configReloadMessage = getConfig().getString("ConfigReloadMsg", "§aConfig reloaded");
         noPermissionMessage = getConfig().getString("NotPermMsg", "§cYou do not have permission.");
 
+        blacklistedDropEgg = new ArrayList<>();
+        List<String> blacklist = getConfig().getStringList("DropEgg.Blacklist");
+        for (String entityName : blacklist) {
+            EntityType entityType = EntityType.fromName(entityName);
+            if (entityType != null) {
+                blacklistedDropEgg.add(entityType);
+            } else {
+                getLogger().warning("Invalid entity type in config: " + entityName);
+            }
+        }
+        
+        blacklistedСurrencyDrop = new ArrayList<>();
+        List<String> blacklistq = getConfig().getStringList("CurrencyDrop.Blacklist");
+        for (String entityName : blacklist) {
+            EntityType entityType = EntityType.fromName(entityName);
+            if (entityType != null) {
+            	blacklistedСurrencyDrop.add(entityType);
+            } else {
+                getLogger().warning("Invalid entity type in config: " + entityName);
+            }
+        }
+        
         commandEntities = new HashMap<>();
         ConfigurationSection entitySection = getConfig().getConfigurationSection("Entity");
         if (entitySection != null) {
@@ -74,34 +98,61 @@ public class mobloot extends JavaPlugin implements Listener {
             }
         }
     }
-
+    
     @EventHandler
-    public void onEntityDeath(EntityDeathEvent event) {
+    public void onDropEgg(EntityDeathEvent event) {
         if (!(event.getEntity().getKiller() instanceof Player)) {
             return;
         }
 
         Player player = event.getEntity().getKiller();
-
+        EntityType entityType = event.getEntityType();
+        
+        // Check if entity is blacklisted
+        if (blacklistedDropEgg.contains(entityType)) {
+            return;
+        }
         // Spawn spawner egg with given chance
-        if (dropEggEnable && new Random().nextDouble() < eggDropChance) {
-            EntityType entityType = event.getEntityType();
+         if (dropEggEnable && new Random().nextDouble() < eggDropChance) {
             ItemStack spawnEgg = new ItemStack(Material.MONSTER_EGG, 1, entityType.getTypeId());
             event.getEntity().getWorld().dropItemNaturally(event.getEntity().getLocation(), spawnEgg);
+        } 
+    }
+    
+    @EventHandler
+    public void onCurrencyDrops(EntityDeathEvent event) {
+        if (!(event.getEntity().getKiller() instanceof Player)) {
+            return;
+        }
+
+        Player player = event.getEntity().getKiller();
+        EntityType entityType = event.getEntityType();
+        
+        if (blacklistedСurrencyDrop.contains(entityType)) {
+            return;
         }
 
         // Drop currency with given chance
         if (new Random().nextDouble() < currencyDropChance) {
-            EntityType entityType = event.getEntityType();
             int min = CurrencyDropsMin;
             int max = CurrencyDropsMax;
             int amount = new Random().nextInt(max - min + 1) + min;
 
             player.sendMessage(getCurrencyMessage(entityType, amount).replace("&", "§"));
         }
+    }
+
+    @EventHandler
+    public void onCommandEntities(EntityDeathEvent event) {
+        if (!(event.getEntity().getKiller() instanceof Player)) {
+            return;
+        }
+
+        Player player = event.getEntity().getKiller();
+        EntityType entityType = event.getEntityType();
+        
 
         // Execute command for specific entities
-        EntityType entityType = event.getEntityType();
         for (Map.Entry<Integer, EntityType> entry : commandEntities.entrySet()) {
             if (entry.getValue() == entityType) {
                 String command = getConfig().getString("Entity." + entry.getKey() + ".Command");
